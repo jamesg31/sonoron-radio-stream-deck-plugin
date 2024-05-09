5d04518701e5995fac5047e0e788e240c09f9465
BuildTools <jamie.gardner25@gmail.com>
2024-05-09 10:37:13 -0700

    toggle scan frequency

--- src/net.gardna.sonoron.sdPlugin/app.js
+++ src/net.gardna.sonoron.sdPlugin/app.js
@@ -2,7 +2,11 @@
 /// <reference path="libs/js/stream-deck.js" />
 
 const setFrequency = new Action("net.gardna.sonoron.set-frequency");
+const toggleScanFrequncy = new Action(
+  "net.gardna.sonoron.toggle-scan-frequency"
+);
 let selectedContext = undefined;
+let scannedFrequencies = [];
 
 /**
  * The first event fired when Stream Deck starts
@@ -29,4 +33,26 @@ setFrequency.onKeyUp(({ action, context, device, event, payload }) => {
   }
   selectedContext = context;
   $SD.setState(context, 1);
+});
+
+toggleScanFrequncy.onKeyUp(({ action, context, device, event, payload }) => {
+  if (payload.state == 0) {
+    scannedFrequencies.push(payload.settings.recv);
+  } else {
+    let index = scannedFrequencies.indexOf(payload.settings.recv);
+    if (index > -1) {
+      scannedFrequencies.splice(index, 1);
+    }
+  }
+  const websocket = new WebSocket("ws://[::1]:33802");
+  websocket.onopen = () => {
+    let request = {
+      type: "set_frequencies_scanned",
+      freqs: scannedFrequencies.map((i) =>
+        i.split(".").map((n) => parseInt(n))
+      ),
+    };
+    websocket.send(JSON.stringify(request));
+    websocket.close();
+  };
 });
